7ecc46961c7562040a4b74f6161841a46a2f124b
a24520186 <b24520186@gmail.com>
2023-02-23 22:52:15 +0800

    first commit

---
+++ js/music-list.js
@@ -0,0 +1,68 @@
+let allMusic = [
+  {
+    name: "North",
+    artist: "JayChou",
+    img: "music-1",
+    src: "jaychou",
+  },
+  {
+    name: "Ikson Anywhere – Ikson",
+    artist: "Audio Library",
+    img: "music-2",
+    src: "music-2",
+  },
+  {
+    name: "Beauz & Jvna - Crazy",
+    artist: "Beauz & Jvna",
+    img: "music-3",
+    src: "music-3",
+  },
+  {
+    name: "Hardwind - Want Me",
+    artist: "Mike Archangelo",
+    img: "music-4",
+    src: "music-4",
+  },
+  {
+    name: "Jim - Sun Goes Down",
+    artist: "Jim Yosef x Roy",
+    img: "music-5",
+    src: "music-5",
+  },
+  {
+    name: "Lost Sky - Vision NCS",
+    artist: "NCS Release",
+    img: "music-6",
+    src: "music-6",
+  },
+  // {
+  //   name: "Lost Sky - Vision NCS",
+  //   artist: "NCS Release",
+  //   img: "music-7",
+  //   src: "music-7",
+  // },
+  // {
+  //   name: "Lost Sky - Vision NCS",
+  //   artist: "NCS Release",
+  //   img: "music-8",
+  //   src: "music-8",
+  // },
+  // {
+  //   name: "Lost Sky - Vision NCS",
+  //   artist: "NCS Release",
+  //   img: "music-9",
+  //   src: "music-9",
+  // },
+  // {
+  //   name: "Lost Sky - Vision NCS",
+  //   artist: "NCS Release",
+  //   img: "music-10",
+  //   src: "music-10",
+  // },
+  // {
+  //   name: "Lost Sky - Vision NCS",
+  //   artist: "NCS Release",
+  //   img: "music-11",
+  //   src: "music-11",
+  // },
+];
